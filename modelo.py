a832ba89045b27335b24d41b980a231534f9788a
thiagosaidm <thiag.said@gmail.com>
2023-11-01 20:38:21 -0300

    "Aula 2 - Avançando em POO - Polimorfismo"

--- modelo.py
+++ modelo.py
@@ -8,7 +8,7 @@ class Programa:
 
     @property
     def likes(self):
-        return self.__likes
+        return self._likes
 
     @likes.setter
     def likes(self, cont_likes):
@@ -20,11 +20,25 @@ class Programa:
     def ver_atributos(self):
         print(" Nome:{}\n Ano:{} \n Likes:{}". format(self._nome, self.ano, self._likes))
 
+    def __str__(self):
+        return "NOME:{} - ANO:{} - LIKES:{}". format(self._nome, self.ano, self._likes)
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao, likes):
         super().__init__(nome, ano, likes)
         self.duracao = duracao
+
+    def __str__(self):
+        return "NOME:{} - ANO:{} - DURAÇÃO: {} min LIKES:{}".format(self._nome, self.duracao, self.ano, self._likes)
+
+
+class Documentario(Programa):
+    def __init__(self, nome, ano, duracao, likes):
+        super().__init__(nome, ano, likes)
+        self.duracao = duracao
+
+    def __str__(self):
+        return "NOME:{} - ANO:{} - DURAÇÃO: {} min LIKES:{}".format(self._nome, self.ano, self.duracao, self._likes)
 
 
 class Serie(Programa):
@@ -32,6 +46,26 @@ class Serie(Programa):
         super().__init__(nome, ano, likes)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return "NOME:{} - ANO:{} - TEMPORADAS: {} Temporadas LIKES:{}".format(self._nome, self.ano, self.temporadas, self._likes)
 
 
+class Anime(Programa):
+    def __init__(self, nome, ano, episodios, likes):
+        super().__init__(nome, ano, likes)
+        self.episodios = episodios
 
+    def __str__(self):
+        return "NOME:{} - ANO:{} - EPISÓDIOS: {} - LIKES:{}".format(self._nome, self.ano, self.episodios,
+                                                                             self._likes)
+
+
+one_piece = Anime("one piece", 1999, 1076, 10)
+piratas_do_caribe = Filme("piratas do caribe", 2004, 160, 5)
+lupin = Serie("lupin", 2020, 3, 1)
+deuses = Documentario("eram os deuses astronautas",2010,180,3)
+
+playlist = [lupin, piratas_do_caribe, one_piece, deuses]
+
+for programa in playlist:
+    print(programa)
